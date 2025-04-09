5d6e5f7055106da32973c86a83aa56fadbe6538f
clement-bramy <clement.bramy@gmail.com>
2025-04-09 19:19:18 +1000

    publish JSON to exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("failed to reach rabbitmq broker")
+	}
+	defer conn.Close()
+	fmt.Println("rabbitmq connection established")
+
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("failed to get channel")
+	}
+
+	pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("interrupt signal received, shutting down..")
 }
